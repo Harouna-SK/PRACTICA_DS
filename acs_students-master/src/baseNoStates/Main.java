82a11a3aa73aec5b855366404b1479f04968aa6f
Uri <oriolbrumos@gmail.com>
2025-10-03 19:51:17 +0200

    Proves al main
    He fet un parell de proves al main

--- acs_students-master/src/baseNoStates/Main.java
+++ acs_students-master/src/baseNoStates/Main.java
@@ -3,10 +3,26 @@ package baseNoStates;
 // Before executing enable assertions :
 // https://se-education.org/guides/tutorials/intellijUsefulSettings.html
 
+import baseNoStates.requests.Request;
+import baseNoStates.requests.RequestArea;
+import baseNoStates.requests.RequestReader;
+
+import java.time.LocalDateTime;
+
 public class Main {
   public static void main(String[] args) {
-    DirectoryDoors.makeDoors();
+    /*DirectoryDoors.makeDoors();
     DirectoryUsers.makeUsers();
-    new WebServer();
+    new WebServer();*/
+
+    Door d100 = new Door("d100");
+    RequestReader request = new RequestReader("95783", "UNLOCK", LocalDateTime.now(), "d100");
+
+    System.out.println(d100);
+
+    d100.processRequest(request);
+
+    System.out.println(d100);
+    System.out.println(request);
   }
 }
